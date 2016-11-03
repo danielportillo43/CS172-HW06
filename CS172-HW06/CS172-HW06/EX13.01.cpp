a08898a5f7560415b1e182eb773641c07c2fe499
Daniel Portillo <dportillo17@my.whitworth.edu>
2016-11-03 15:34:46 -0700

    initial

---
+++ CS172-HW06/CS172-HW06/EX13.01.cpp
@@ -0,0 +1,9 @@
+//
+//  EX13.01.cpp
+//  CS172-HW06
+//
+//  Created by Daniel Portillo on 11/3/16.
+//  Copyright © 2016 Daniel Portillo. All rights reserved.
+//
+
+#include "EX13.01.hpp"
---
+++ CS172-HW06/CS172-HW06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS172-HW06
+//
+//  Created by Daniel Portillo on 11/3/16.
+//  Copyright © 2016 Daniel Portillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
